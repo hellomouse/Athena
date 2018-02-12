c0fc42b2ca2d30ce785562d106e91e86d704df9a
wolfy1339 <webmaster@wolfy1339.com>
2018-02-12 11:27:16 -0500

    Change winston colors to the ones from cli-color

--- utils/logging.js
+++ utils/logging.js
@@ -1,8 +1,26 @@
 const winston = require("winston");
+const clc = require("cli-color");
 
-module.exports = new (winston.Logger)({
-    transports: [
-        new (winston.transports.Console)({ colorize: true }),
-        new (winston.transports.File)({ filename: "messages.log" })
-    ]
+function timestamps () {
+
+    return `[${new Date().toLocaleString()}]`;
+
+}
+
+const logger = new (winston.Logger)({
+     transports: [
+         new (winston.transports.Console)({ colorize: true, timestamp: timestamps }),
+         new (winston.transports.File)({ filename: "messages.log", timestamp: true })
+     ]
 });
+
+winston.addColors({
+    silly: clc.blue,
+    debug: clc.cyan,
+    info: clc.green,
+    warn: clc.yellow,
+    error: clc.red,
+    verbose: clc.magenta
+});
+
+module.exports = logger;
