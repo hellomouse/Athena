4f51204ccc39f855163de53758ad4a6e331552e5
wolfy1339 <webmaster@wolfy1339.com>
2018-02-20 13:53:07 -0500

    Remove new-line between JSDoc comment and function

--- utils/messageParser.js
+++ utils/messageParser.js
@@ -21,8 +21,8 @@ class User {
  * @param {string} string - The string you want split.
  * @param {string} sep - The seperator by which you want the string to be split
  * @param {number} maxCount - The maximum count of times you wish to split
+ * @return {array} - An Array containing the split string
  */
-
 function split(string, sep, maxCount) {
     string = string.split(sep);
     const first = string.slice(0, maxCount);
