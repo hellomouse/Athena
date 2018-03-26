10a7315ae41743098585f7ab763a0b42658fd165
wolfy1339 <webmaster@wolfy1339.com>
2018-03-26 08:30:45 -0400

    Improvements and fixes to formatting stripper
    Use RegExp for all codes because a message can contain many of them altogether.
    Also, put the last regex into the Array

--- utils/general.js
+++ utils/general.js
@@ -5,13 +5,11 @@
 */
 function strip_formatting(msg) {
     /* eslint-disable no-control-regex */
-    let ccodes = ['\x0f', '\x16', '\x1d', '\x1f', '\x02'];
-
-    ccodes.push(new RegExp('\\x03([1-9][0-6]?)?,?([1-9][0-6]?)?', 'g'));
+    let ccodes = ['\\x0f', '\\x16', '\\x1d', '\\x1f', '\\x02', '\\x03([1-9][0-6]?)?,?([1-9][0-6]?)?'];
     /* eslint-enable no-control-regex */
 
     for (let cc of ccodes)
-        msg = msg.replace(cc, '');
+        msg = msg.replace(new RegExp(cc, 'g'), '');
 
     return msg;
 }
