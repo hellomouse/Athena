830c56a7af0282dc4956d4793013348445ed989b
wolfy1339 <webmaster@wolfy1339.com>
2018-04-30 19:40:48 -0400

    Add the basic functionality plugins
    Now @BWBellaairs has no excuse of not having them

---
+++ plugins/channel.js
@@ -0,0 +1,44 @@
+function _get_info(event, args) {
+    let target, nick;
+
+    if (args.length >= 1) {
+        if (args[0].startsWith('#')) {
+            target = args[0];
+            args.shift();
+            nick = args.length ? args : event.source.nick;
+        } else {
+            target = event.target;
+            nick = args;
+        }
+    } else {
+        target = event.target;
+        nick = event.source.nick;
+    }
+
+    return [target, nick];
+}
+function op(bot, event, irc, args) {
+    let [target, nick] = _get_info(event, args);
+
+    irc.op(target, nick);
+}
+op.opts = {
+    perms: [false, true, false],
+    hide: false,
+    min_args: 0
+};
+function deop(bot, event, irc, args) {
+    let [target, nick] = _get_info(event, args);
+
+    irc.deop(target, nick);
+}
+deop.opts = {
+    perms: [false, true, false],
+    hide: false,
+    min_args: 0
+};
+
+module.exports = {
+    op,
+    deop
+};
--- plugins/general.js
+++ plugins/general.js
@@ -1,4 +1,39 @@
+const { check_perms } = require('../utils/permissions');
+
 /* eslint-disable require-jsdoc */
+function todo(bot, event, irc, args) {
+    let has_perms = check_perms(bot.config, event.source.host, event.target, [true, false, false]);
+
+    if (args[0] === 'add' && has_perms) {
+        bot.todo.push(args.slice(1).join(' '));
+    } else if (args[0] === 'remove' && has_perms) {
+        let index = parseInt(args[1]);
+        let text = '';
+
+        for (let i of bot.todo[index]) {
+            text += `\u0336${i}`;
+        }
+        text += '- Done!';
+        bot.todo[index] = text;
+    } else {
+        irc.reply(event, 'To-do List:');
+        if (bot.todo.length === 0) {
+            irc.reply(event, 'Empty');
+
+            return;
+        }
+        for (let i of Object.entries(bot.todo)) {
+            irc.reply(event, i.join('. '));
+        }
+    }
+}
+
+todo.opts = {
+    perms: [false, false, false],
+    hide: false,
+    min_args: 0
+};
+
 function ping(bot, event, irc, args) {
     irc.reply(event, 'Pong');
 }
@@ -8,6 +43,69 @@ ping.opts = {
     min_args: 0,
     hide: false
 };
+
+function join(bot, event, irc, args) {
+    irc.join(args[0], args[1]);
+}
+join.opts = {
+    perms: [false, true, true],
+    min_args: 1,
+    hide: false
+};
+
+
+function quit(bot, event, irc, args) {
+    irc.quit(args.join(' ') || 'Athena - https://github.com/BWBellairs/Athena');
+}
+
+quit.opts = {
+    perms: [false, true, true],
+    min_args: 0,
+    hide: false
+};
+
+function Eval(bot, event, irc, args) {
+    try {
+        let result = eval(args.join(' '));
+
+        try {
+            if (result instanceof Object && JSON.stringify(result) !== undefined) {
+                irc.reply(event, JSON.stringify(result).replace(/\r\n|\n|\r/g, ''));
+            } else
+                irc.reply(event, String(result).replace(/\r\n|\n|\r/g, ''));
+        } catch (e) {
+            irc.reply(event, result);
+        }
+    } catch (e) {
+        irc.reply(event, e.toString());
+    }
+}
+
+function list(bot, event, irc, args) {
+    irc.reply(event, Object.keys(bot.plugins).filter(x => {
+        if (x !== 'bot') {
+            return !bot.plugins[x].opts.hide;
+        }
+    }).join(', '));
+}
+
+list.opts = {
+    perms: [false, false, false],
+    min_args: 0,
+    hide: false
+};
+
+Eval.opts = {
+    perms: [false, false, true],
+    min_args: 1,
+    hide: false
+};
+
 module.exports = {
-    ping
+    todo,
+    ping,
+    quit,
+    eval: Eval,
+    list,
+    join
 };
