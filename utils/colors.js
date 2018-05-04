ecdf00c469c5d87884cfbb8765691c8373a5a2c0
wolfy1339 <webmaster@wolfy1339.com>
2018-05-04 16:05:30 -0400

    Add a proof of concept for a new color class

--- utils/colors.js
+++ utils/colors.js
@@ -37,10 +37,223 @@ const colors = {
 const _rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'violet'];
 
 /**
-* Colors a message with rainbow colors
-* @param {string} msg - The message you want to add rainbow colors too
-* @return {string} - Returns your message returned with rainbow coloring
+* Colors a message
 */
+class Color extends String {
+    /**
+    * @param {string} message - The message you want to add rainbow colors too
+    */
+    constructor(message) {
+        super(message);
+    }
+
+    white() {
+        super(`${colors.WHITE}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    black() {
+        super(`${colors.BLACK}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    navy() {
+        super(`${colors.NAVY}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    green() {
+        super(`${colors.GREEN}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    red() {
+        super(`${colors.RED}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    brown() {
+        super(`${colors.BROWN}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    maroon() {
+        super(`${colors.MAROON}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    purple() {
+        super(`${colors.PURPLE}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    violet() {
+        super(`${colors.VIOLET}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    orange() {
+        super(`${colors.ORANGE}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    yellow() {
+        super(`${colors.YELLOW}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    lightgreen() {
+        super(`${colors.LIGHTGREEN}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    lime() {
+        super(`${colors.LIME}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    teal() {
+        super(`${colors.TEAL}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    bluecyan() {
+        super(`${colors.BLUECYAN}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    cyan() {
+        super(`${colors.CYAN}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    aqua() {
+        super(`${colors.AQUA}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    blue() {
+        super(`${colors.BLUE}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    royal() {
+        super(`${colors.ROYAL}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    lightpurple() {
+        super(`${colors.LIGHTPURPLE}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    pink() {
+        super(`${colors.PINK}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    fuchsia() {
+        super(`${colors.FUCHSIA}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    grey() {
+        super(`${colors.GREY}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    gray() {
+        super(`${colors.GRAY}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    lightgray() {
+        super(`${colors.LIGHTGRAY}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    lightgrey() {
+        super(`${colors.LIGHTGREY}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    silver() {
+        super(`${colors.SILVER}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+
+    bold() {
+        super(`${colors.BOLD}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    italic() {
+        super(`${colors.ITALIC}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    underline() {
+        super(`${colors.UNDERLINE}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    reverse() {
+        super(`${colors.ITALIC}${this}${colors.NORMAL}`);
+
+        return this;
+    }
+
+    /**
+    * Colors a message with rainbow colors
+    * @return {string} - Returns your message returned with rainbow coloring
+    */
+    rainbow() {
+        let i = 0;
+        let colored = '';
+
+        for (let character of this) {
+            if (i > (_rainbow.length - 1)) // We substract one because i starts at 0 and rainbow.length at 1
+                i = 0;
+
+            colored += `${colors[_rainbow[i].toUpperCase()]}${character}`;
+            i += 1;
+        }
+
+        return colored.concat('\x0F');
+    }
+}
+
 function make_rainbow(msg) {
     let i = 0;
     let colored = '';
@@ -74,12 +287,7 @@ function add_background(msg, bg) {
     }
 }
 
-/**
-* Colors a message with rainbow colors
-* @param {string} msg - The message you want to add rainbow colors too
-* @param {string} style - The message you want to add rainbow colors too
-* @return {string} - Returns your message returned with rainbow coloring
-*/
+
 function stylize(msg, style) {
     if (style !== null) {
         return `${colors[style.toUpperCase()]}${msg}`;
