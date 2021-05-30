33c73ca41ef1b06d3ad3452cd11f8694762ccc7b
wolfy1339 <webmaster@wolfy1339.com>
2021-05-30 12:10:06 -0400

    style: lint .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    env: {
+        es6: true,
+        node: true
     },
-    "parserOptions": {
-        "ecmaVersion": 9,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 9,
+        sourceType: 'module'
     },
-    "extends": ["eslint:recommended", "@hellomouse/eslint-config-wolfy1339"],
-    "rules": {
-        "indent":  ["error", 4]
+    extends: ['eslint:recommended', '@hellomouse/eslint-config-wolfy1339'],
+    rules: {
+        indent: ['error', 4]
     }
 };
